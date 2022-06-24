718dcbce76001efb889fdce6ecc8fd6fe3190e7b
starmind <dkload1588@gmail.com>
2022-06-24 11:04:50 +0900

    Router & create components & helmet &global styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { HelmetProvider } from "react-helmet-async";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Detail } from "./components/pages/Detail/Detail";
+import { Home } from "./components/pages/Home/Home";
+import { NotFound } from "./components/pages/NotFound";
+import { Search } from "./components/pages/search/Search";
+import { GlobalStyled } from "./styles/globalStyle";
+
 function App() {
-  return <div>Apple</div>;
+  return (
+    <HelmetProvider>
+      <Router>
+        <GlobalStyled />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
+  );
 }
 
 export default App;
